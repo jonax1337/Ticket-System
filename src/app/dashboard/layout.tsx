2ff9239d9640da67512258344cb4d4635b3ed5ce
Jonas Laux <jonas.laux@hotmail.com>
2025-08-01 20:36:34 +0200

    fix: remove unnecessary padding from main container in DashboardLayout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -64,7 +64,7 @@ export default async function DashboardLayout({
               logoUrl={settings.logoUrl}
               hideAppName={settings.hideAppName}
             />
-            <main className="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <main className="container mx-auto py-6">
               {children}
             </main>
           </div>
